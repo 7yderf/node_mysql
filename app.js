e7cf812d80e293c57d7c91783d15549944ef4860
Fredy <fredy.nazario@eossoftware.mx>
2021-10-07 14:11:21 -0500

    2

--- app.js
+++ app.js
@@ -17,10 +17,7 @@ const connection = mysql.createPool({
   database: 'heroku_cb75fcac67db9c1'
 });
 
-pool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
-});
+
 
 // Route
 app.get('/', (req, res) => {
