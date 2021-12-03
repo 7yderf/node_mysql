42f3711b04828ee887a1e18d9a3cb9ac607b5f24
Fredy <fredy.nazario@eossoftware.mx>
2021-12-03 12:55:23 -0600

    cambio a api sin base de datos

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+
+let users = [
+    {
+      id: 1,
+      user: 'Juan',
+    },
+    {
+      id: 2,
+      user: 'Agustina',
+    },
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Servidor con express</h1>')
+})
+
+app.get('/api/users', (request, response) => {
+    response.json(users)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`El servidor está levantado en el puerto ${PORT}`) 
+})
+
+
+// let count = 0;
+// function masuno(){
+//   count+=1;
+// }
+// setInterval(function(){ 
+ 
+//   masuno()
+//   console.log(count + " seg") 
+
+//   if(count == 50){
+//     console.log("esta por acabarse")
+//   }
+// }, 1000);
